6fea60f002ef9eeea9a2a8d628efda27f9182e99
neozeg <neozeg@qq.com>
2016-06-14 17:22:06 +0800

    Add NetworkServer Class

--- app/src/main/java/com/mupro/socket_test/MainActivity.java
+++ app/src/main/java/com/mupro/socket_test/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Server = new NetworkServer();
+        Server.main(null);
     }
 
     @Override
